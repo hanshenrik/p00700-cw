ce7c0cbffcc331045b90710dea2c41012e12337a
hanshenrik <hh.gronsleth@gmail.com>
2015-04-24 16:19:41 +0100

    added some space

--- js/task-2-course-and-module-selection.js
+++ js/task-2-course-and-module-selection.js
@@ -110,6 +110,7 @@ $(document).ready(function() {
           })
         }
       })
+      
       $( '#selectAllButton' ).click(function() {
         // clear table
         $( '#schedule-table tbody tr' ).remove()
